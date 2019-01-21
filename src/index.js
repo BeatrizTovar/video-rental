6424278dfaa6f4f047d81bd70251270892a22fff
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-20 19:48:08 -0800

    adding files to .gitignore

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-class Person { 
-    constructor(name) {
-        this.name = name;
-    }
-
-    walk() {
-      console.log("walk");
-    }   
-}
-
-const person = new Person('Bea')
-person. //demonstrates what methods are availe for the new object of class person
-
- class Teacher extends Person {
-     constructor(name, degree){
-         super(name);
-         this.degree = degree
-     }
-     teach() {
-         console.log("teach");
-     }
- }
+import Teacher, { promote } from './teacher';
+// import React, { Component } from 'react'; 
 
  const teacher = new Teacher('Bea', "AA")
+ teacher.teach()
