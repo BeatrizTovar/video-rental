aa4bb947e4be74cfcaa8ae191cf7199666930412
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-30 22:56:42 -0800

    added Delete functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,38 +1,51 @@
-import React from 'react';
-import { getMovies } from '../services/fakeMovieService';
+import React from "react";
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends React.Component {
-    state = {
-        movies: getMovies() //temporary
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.movies.map(movie => (
-                            <tr>
-                               <td>{movie.title}</td>
-                               <td>{movie.genre.name}</td>
-                               <td>{movie.numberInStock}</td>
-                               <td>{movie.dailyRentalRate}</td>
-                            </tr>
-                            ))}
+  state = {
+    movies: getMovies() //temporary
+  };
 
-                    </tbody>
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies }); //avoid repetition i.e {movies: movies}
+  };
 
-                </table>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Movies;
