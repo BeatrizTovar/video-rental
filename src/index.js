4c689f94da6220e4c3cb95cda98b7f68af9036f7
Beatriz Tovar <beatrizctovar@gmail.com>
2019-03-04 21:29:06 -0800

    upgrading packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
@@ -10,7 +10,7 @@ import Movies from "./components/movies";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Movies />
   </BrowserRouter>,
   document.getElementById("root")
 );
