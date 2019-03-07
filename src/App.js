106650f7e2ec7a8d8773dd9d38526fc71171bf0a
Beatriz Tovar <beatrizctovar@gmail.com>
2019-03-06 21:53:37 -0800

    adding more React Router to navbar

--- src/App.js
+++ src/App.js
@@ -1,74 +1,24 @@
 import React, { Component } from "react";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
-// import Movies from './components/movies'
-import NavBar from "./components/navbar";
-import Counters from "./components/counters";
 
 class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  constructor() {
-    super();
-    console.log("App-Constructor");
-  }
-
-  componentDidMount() {
-    console.log("App-mounted");
-  }
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDecrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value--;
-    this.setState({ counters });
-    console.log(counters);
-  };
   render() {
-    console.log("App-rendered");
     return (
-      <React.Fragment>
-        <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
-        />
-        <main className="container">
-          <Counters
-            counters={this.state.counters}
-            onReset={this.handleReset}
-            onIncrement={this.handleIncrement}
-            onDelete={this.handleDelete}
-            onDecrement={this.handleDecrement}
-          />
-        </main>
-      </React.Fragment>
+      <main className="container">
+        {/* <Movies /> */}
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" to="/movies" />
+        </Switch>
+      </main>
     );
   }
 }
