16f0cd4c711e50b7676249f85cd3afd409b6687d
Beatriz Tovar <beatrizctovar@gmail.com>
2019-03-07 22:15:24 -0800

    added default window reload

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  state = {};
+  handleSubmit = e => {
+    e.preventDefault();
+    //call server
+    console.log("submit");
+  };
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
