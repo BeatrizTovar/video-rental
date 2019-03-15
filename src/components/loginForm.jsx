cbde875e84d8265f1d8166d5a34cc479d9a09c93
Beatriz Tovar <beatrizctovar@gmail.com>
2019-03-14 18:18:43 -0700

    added joi and created validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import Input from "../common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
     errors: {}
   };
-
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
   validate = () => {
-    return { username: "username is required" };
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = e => {
@@ -17,19 +32,30 @@ class LoginForm extends Component {
     // const ussername = document.getElementById('username').value VANILLA JS
 
     const errors = this.validate();
-    this.setState({ errors });
+    this.setState({ errors: errors || {} }); //never be null
     if (errors) return;
 
     console.log("submit");
   };
 
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -39,6 +65,7 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
 
           <div className="form-group">
@@ -47,6 +74,7 @@ class LoginForm extends Component {
               value={account.password}
               label="Password"
               onChange={this.handleChange}
+              error={errors.password}
             />
           </div>
           <button className="btn btn-primary">Login</button>
